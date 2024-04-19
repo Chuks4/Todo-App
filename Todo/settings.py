d8fba91020396ef4e9ec63d191dbc8ea52f0e46d
Agha Chukwuemeka <chukwuemekaagha733@gmail.com>
2024-04-19 11:15:45 +0100

    Modified settings.py file

--- Todo/settings.py
+++ Todo/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOST", "localhost",).split(",")
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'todo-app-5upk.onrender.com']
 
 # Application definition
 INSTALLED_APPS = [
