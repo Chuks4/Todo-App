01d4242b150283668eef196906190e4ceb9e0197
Agha Chukwuemeka <chukwuemekaagha733@gmail.com>
2024-04-20 21:24:39 +0100

    Modified settings.py file

--- Todo/settings.py
+++ Todo/settings.py
@@ -12,12 +12,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY", 'django-insecure-#_)5is8n0)2o9ne8ez*5x$p%j2wc!w9#g&$9pui)r9zkzz!pkv')
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY", "django-insecure-#_)5is8n0)2o9ne8ez*5x$p%j2wc!w9#g&$9pui)r9zkzz!pkv"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'todo-app-5upk.onrender.com']
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "todo-app-5upk.onrender.com"]
 
 # Application definition
 INSTALLED_APPS = [
@@ -65,7 +67,12 @@ WSGI_APPLICATION = "Todo.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
 
 if not DEBUG:
     DATABASES = {"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))}
@@ -73,12 +80,6 @@ if not DEBUG:
 
 else:
     ALLOWED_HOSTS = []
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
-    }
     print("local")
 
 
